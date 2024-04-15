99c221917581abca870564bb6bae2d922ede3267
Erik <erikjhow.ed@gmail.com>
2024-04-14 21:48:43 -0300

    clean

--- backend/server/app.js
+++ backend/server/app.js
@@ -10,19 +10,13 @@ const wss = new WebSocketServer({port: process.env.PORT || 8080 })
 wss.on("connection", (ws) => {
     ws.on("error", console.error)
 
-    // ws.send("Seja bem vindo!")
     
     ws.on("message", (data) => {
         // console.log(data.toString())
         messages.push(data.toString())
         console.log(messages)
 
-        // messages.forEach((data) => {
-        //     wss.clients.forEach((client) => {
-        //         client.send(data.toString())
-        //     })
-        // })
-        
+
         wss.clients.forEach((client) => {
             client.send(data.toString())
         })
